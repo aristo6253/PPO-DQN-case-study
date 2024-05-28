dfa4bb184a673578b14f6324133f9138c73388ba
A-B-A-K <102583994+A-B-A-K@users.noreply.github.com>
2024-05-28 16:12:38 +0200

    update

--- dqn/models/Q_agent.py
+++ dqn/models/Q_agent.py
@@ -1,33 +1,16 @@
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.autograd import Variable
-from typing import List, Tuple
-
-import collections
-from collections import namedtuple, deque
-import tqdm
-import matplotlib.pyplot as plt
+from collections import namedtuple
 import random
 import gymnasium as gym
-
-from IPython.display import clear_output
-from IPython import display
 
 from .base_architecture import Q_model
 from utils.memory import ExpMem
 
 Recording = namedtuple('Recording', ('state', 'action', 'next_state', 'reward'))
 
-#actions = 2
-#states = 4
-
 class Architecture:
-    def __init__(self, env_name, lr=5e-2, render=False, actions=2, states=4, mem_size=10000): #changes mem_size to 1000 
-        # Class constructor
-        #print(f"mem size is {mem_size = }")
+    def __init__(self, env_name, lr=5e-2, render=False, actions=2, states=4, mem_size=10000): #alexmod mem_size
         self.env = gym.make(env_name)
         self.lr = lr
         self.learning_net = Q_model(self.env, self.lr, actions=actions, states=states)
@@ -41,7 +24,6 @@ class Architecture:
         self.c = 0
         
     def save_in_mem(self):
-        # We have to initialize the experience memory with a number of episodes
         cnt = 0
         terminated = False
         truncated = False
@@ -61,16 +43,13 @@ class Architecture:
                 next_state = None
             else:
                 next_state = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0)
-                #print(f"{next_state.shape = }")
-            
-            # Store new experience into memory
+
             transition = Recording(state, action, next_state, reward)
             self.mem.mem.append(transition)
             state = next_state
             cnt += 1
         
     def e_greedy(self, q_vals, epsilon=0.05):
-        #print(f"{q_vals = }")
         p = random.random()
         if p > epsilon:
             with torch.no_grad():
@@ -79,17 +58,14 @@ class Architecture:
             return torch.tensor([[self.env.action_space.sample()]], dtype=torch.long)
     
     def train(self):
-        # Train the Q-network using Deep Q-learning.
         state, _ = self.env.reset()
         state = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
         terminated = False
         truncated = False
 
-        # Loop until reaching the termination state
         while not (terminated or truncated):
             with torch.no_grad():
                 q_values = self.learning_net.net(state)
-            #print(f"{q_values = }")
             action = self.e_greedy(q_values).reshape(1,1).item()
             next_state, reward, terminated, truncated, _ = self.env.step(action)
             reward = torch.tensor([reward])
@@ -99,7 +75,6 @@ class Architecture:
             else: 
                 next_state = torch.tensor(next_state, dtype=torch.float32).unsqueeze(0)
             
-            # Store the new experience
             transition = Recording(state, action, next_state, reward)
             self.mem.mem.append(transition)
             state = next_state
@@ -107,28 +82,22 @@ class Architecture:
             transitions = self.mem.sample_random(self.batch_size)
             batch = Recording(*zip(*transitions))
             non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool)
-            #non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
             non_final_next_states = [s for s in batch.next_state if s is not None]
             if len(non_final_next_states) == 0:
-                return  # Skip this batch or handle differently
+                return  
 
             non_final_next_states = torch.cat(non_final_next_states)
 
             state_batch = torch.cat(batch.state)
-            action_batch = torch.tensor(batch.action, dtype=torch.int64).unsqueeze(1)  # Convert to tensor and unsqueeze
+            action_batch = torch.tensor(batch.action, dtype=torch.int64).unsqueeze(1)
             reward_batch = torch.cat(batch.reward)
 
-            # Get current and next state values
-            #print(f"{state_batch.shape = } \n{action_batch.shape = }")
-            state_action_values = self.learning_net.net(state_batch).gather(1, action_batch) # extract values corresponding to the actions Q(S_t, A_t)
+            state_action_values = self.learning_net.net(state_batch).gather(1, action_batch) 
             next_state_values = torch.zeros(self.batch_size)
             
             with torch.no_grad():
-                # no next_state_value update if an episode is terminated (next_satate = None)
-                # only update the non-termination state values (Ref: https://gymnasium.farama.org/tutorials/gymnasium_basics/handling_time_limits/)
                 next_state_values[non_final_mask] = self.target_net.net(non_final_next_states).max(1)[0] # extract max value
                 
-            # Update the model
             expected_state_action_values = (next_state_values * self.gamma) + reward_batch
             criterion = torch.nn.MSELoss()
             loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
@@ -136,15 +105,12 @@ class Architecture:
             loss.backward()
             self.learning_net.optimizer.step()
 
-            # Update the target Q-network in each 50 steps
             self.c += 1
             if self.c % 50 == 0:
                 self.target_net.net.load_state_dict(self.learning_net.net.state_dict())
             
 
     def test(self, model_file=None):
-        # Evaluates the performance of the agent over 20 episodes.
-
         max_t = 1000
         state, _ = self.env.reset()
         rewards = []
@@ -153,7 +119,7 @@ class Architecture:
             state = torch.from_numpy(state).float().unsqueeze(0)
             with torch.no_grad():
                 q_values = self.learning_net.net(state)
-            action = torch.argmax(q_values).item() #self.e_greedy(q_values)
+            action = torch.argmax(q_values).item()
             state, reward, terminated, truncated, _ = self.env.step(action)
             rewards.append(reward)
             if terminated or truncated:
--- dqn/models/base_architecture.py
+++ dqn/models/base_architecture.py
@@ -1,25 +1,9 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-from torch.autograd import Variable
-from typing import List, Tuple
-
-import collections
-from collections import namedtuple, deque
-import tqdm
-import matplotlib.pyplot as plt
-import random
-import gymnasium as gym
-
-from IPython.display import clear_output
-from IPython import display
-
 
 class NeuralNetwork(nn.Module):
     def __init__(self, in_size, out_size):
-        # Init the network 
         super(NeuralNetwork, self).__init__()
         self.linear1 = nn.Linear(in_size, 128)
         self.linear2 = nn.Linear(128, 256)
@@ -29,14 +13,12 @@ class NeuralNetwork(nn.Module):
         self.relu2 = nn.ReLU()
         self.relu3 = nn.ReLU()
 
-        # Xavier init the weights
         nn.init.xavier_uniform_(self.linear1.weight)
         nn.init.xavier_uniform_(self.linear2.weight)
         nn.init.xavier_uniform_(self.linear3.weight)
         nn.init.xavier_uniform_(self.outLayer.weight)
     
     def forward(self, inputs):
-        # Forward pass of the network
         out = self.relu1(self.linear1(inputs))
         out = self.relu2(self.linear2(out))
         out = self.relu3(self.linear3(out))
@@ -46,7 +28,6 @@ class NeuralNetwork(nn.Module):
 
 class Q_model:
     def __init__(self, env, lr, states, actions, logdir=None):
-        # Define Q-network with specified architecture
         self.net = NeuralNetwork(states, actions)
         self.env = env
         self.lr = lr 
@@ -54,7 +35,6 @@ class Q_model:
         self.optimizer = optim.AdamW(self.net.parameters(), lr=self.lr)
 
     def load_model(self, model_file):
-        # Load pre-trained model from a file
         return self.net.load_state_dict(torch.load(model_file))
 
 
